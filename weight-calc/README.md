4c3c9eacd94b3888b267d8cd5410f9fbc27a7230
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2025-06-02 17:54:38 -0500

    add weight-calc boilerplate

---
+++ weight-calc/README.md
@@ -0,0 +1,5 @@
+# How much can I fit in my truck?
+
+## Setup forked from nik0kin/planting-calendar
+
+..hastely
---
+++ weight-calc/src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import './App.css';
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <h1>
+        How much can I hold in my <strong>Truck?</strong>
+      </h1>
+      <p>Hello World</p>
+    </>
+  );
+};
